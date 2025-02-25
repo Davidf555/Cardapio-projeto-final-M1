51425c639f97c68d120293f3031a056372c1cc26
vlaraa11 <vl7829765@gmail.com>
2025-02-25 19:32:41 -0300

    Correção pt2

--- src/cards e carrinho.js
+++ src/cards e carrinho.js
@@ -1,13 +1,13 @@
 var produtos = [
     { img: "imagens/Baton-removebg-preview.png", titulo: "Baton", descricao: "Chocolate ao leite com 30 unidades", preco: 62.89 },
-    { img: "imagens/caixa_prestigio-removebg-preview.png", titulo: "Caixa de prestigio", descricao: "Chocolate com recheio de coco com 30 unidades", preco: 51.40 },
+    { img: "imagens/caixa_prestigio-removebg-preview.png", titulo: "Caixa de prestígio", descricao: "Chocolate com recheio de coco com 30 unidades", preco: 51.40 },
     { img: "imagens/Caixa_Tortuguita-removebg-preview.png", titulo: "Caixa de tortuguita", descricao: "Tortuguita de baunilha com 24 unidades", preco: 53.45 },
     { img: "imagens/Ferrero_Rocher-removebg-preview.png", titulo: "Ferrero Rocher", descricao: "Bombom de avelã com 8 unidades", preco: 55.86 },
     { img: "imagens/Snickers-removebg-preview.png", titulo: "Snickers", descricao: "Barra de chocolate com 20 unidades", preco: 34.50 },
     { img: "imagens/Treno_Leite-removebg-preview.png", titulo: "Trento de leite", descricao: "Chocolate ao leite caixa com 16 unidades", preco: 45.80 },
     { img: "imagens/Treno_Massino-removebg-preview.png", titulo: "Trento de massino", descricao: "Chocolate ao leite caixa com 15 unidades", preco: 44.40 },
     { img: "imagens/Treno_Morango-removebg-preview.png", titulo: "Trento de morango", descricao: "Chocolate sabor morango caixa com 16 unidades", preco: 43.60 },
-    { img: "imagens/Milkaoreo-removebg-preview.png", titulo: "Milkaoreo", descricao: "Chocolate ao leite com pedaços da bolacha Oreo", preco: 12.92 }
+    { img: "imagens/Milkaoreo-removebg-preview.png", titulo: "Milka oreo", descricao: "Chocolate ao leite com pedaços da bolacha Oreo", preco: 12.92 }
 ];
 
 var listaCarrinho = []; // Armazena os itens adicionados ao carrinho
